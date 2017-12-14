596e34d6973fe2b10d5e99d94fd342b882f9f645
kcwiakala <konrad.cwiakala@gmail.com>
2017-12-14 20:56:39 +0100

    Added missing test for package exports

---
+++ test/index.js
@@ -0,0 +1,152 @@
+
+const _ = require('lodash');
+const expect = require('chai').expect;
+const test = require('./izi-test');
+const {Mock, Matcher} = require('jsmock');
+
+const child_process = require('child_process');
+const parser = require('../lib/parser');
+
+describe('package', () => {
+  
+  let cpMock = new Mock(child_process);
+  let parserMock = new Mock(parser);
+
+  test.release('../lib/df');
+  let sut = test.rerequire('../index');
+
+  const result1 = {
+    '/': {
+      filesystem: '/dev/sda1',
+      blocks: 245084444,
+      used: 54700392,
+      available: 177864784,
+      fill: 0.24 
+    }
+  }
+
+  after(() => {
+    cpMock.cleanup();
+    parserMock.cleanup();
+  });
+
+  it('Should propagate error from child_process execution', (done) => {
+    cpMock.expectCall('exec')
+      .willOnceInvoke(new Error('Command not found'));
+
+    sut(err => {
+      expect(err).to.be.instanceOf(Error);
+      expect(err.message).to.be.equal('Command not found');
+      cpMock.verify(done);
+    });
+  });
+
+  it('Should propagate error from parser', (done) => {
+    cpMock.expectCall('exec')
+      .willOnceInvoke(null, 'Some test output');
+
+    parserMock.expectCall('parse', 'Some test output')
+      .willOnce(() => {
+        throw new Error('Something went wrong during parsing')
+      });
+
+    sut(err => {
+      expect(err).to.be.instanceOf(Error);
+      expect(err.message).to.be.equal('Something went wrong during parsing');
+      parserMock.verify();
+      cpMock.verify(done);
+    });
+  });
+
+  it('Should call df with no additional parameters if no options provided', (done) => {
+    cpMock.expectCall('exec')
+      .matching('df', Matcher.FUNCTION)
+      .willOnceInvoke(new Error('some error'));
+
+    sut(err => {
+      expect(err).to.be.instanceOf(Error);
+      cpMock.verify(done);
+    });
+  });
+
+  it('Should pass first argument to df call if string given', (done) => {
+    cpMock.expectCall('exec')
+      .matching('df /some/test/path', Matcher.FUNCTION)
+      .willOnceInvoke(new Error('some error'));
+
+    sut('/some/test/path', err => {
+      expect(err).to.be.instanceOf(Error);
+      cpMock.verify(done);
+    });
+  });
+
+  it('Should pass path argument to df call if provided in option object', (done) => {
+    cpMock.expectCall('exec')
+      .matching('df /some/test/path', Matcher.FUNCTION)
+      .willOnceInvoke(new Error('some error'));
+    const options = {path: '/some/test/path'};
+    sut(options, err => {
+      expect(err).to.be.instanceOf(Error);
+      cpMock.verify(done);
+    });
+  });
+
+  it('Should pass path argument to df call if provided in option object', (done) => {
+    cpMock.expectCall('exec')
+      .matching('df /some/test/path', Matcher.FUNCTION)
+      .willOnceInvoke(new Error('some error'));
+
+    sut({path: '/some/test/path'}, err => {
+      expect(err).to.be.instanceOf(Error);
+      cpMock.verify(done);
+    });
+  });
+
+  describe('formatting', () => {
+    it('Should change base if provided in option object', done => {
+      cpMock.expectCall('exec')
+        .matching('df', Matcher.FUNCTION)
+        .willTwiceInvoke(null, 'output');
+
+      parserMock.expectCall('parse', 'output')
+        .willOnce(_.cloneDeep(result1))
+        .willOnce(_.cloneDeep(result1));
+    
+      sut({base: 'G'}, (err, out) => {
+        expect(err).to.be.null;
+        expect(out['/'].blocks).to.be.equal(234);
+      });
+
+      sut({base: 'M'}, (err, out) => {
+        expect(err).to.be.null;
+        expect(out['/'].blocks).to.be.equal(239340);
+
+        parserMock.verify();
+        cpMock.verify(done);
+      });
+    });
+
+    it('Should change number of decimal places if provided in option object', done => {
+      cpMock.expectCall('exec')
+      .matching('df', Matcher.FUNCTION)
+      .willTwiceInvoke(null, 'output');
+
+    parserMock.expectCall('parse', 'output')
+      .willOnce(_.cloneDeep(result1))
+      .willOnce(_.cloneDeep(result1));
+  
+    sut({base: 'G', decimals: 2}, (err, out) => {
+      expect(err).to.be.null;
+      expect(out['/'].used).to.be.equal(52.17);
+    });
+
+    sut({base: 'M', decimals: 1}, (err, out) => {
+      expect(err).to.be.null;
+      expect(out['/'].used).to.be.equal(53418.4);
+
+      parserMock.verify();
+      cpMock.verify(done);
+    });
+    });
+  });
+});
